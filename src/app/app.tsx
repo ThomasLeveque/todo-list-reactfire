7c67da50eadf46697b2b4a3724a58a3f7f330b33
Thomas Leveque <thomasl5895@gmail.com>
2020-04-25 17:09:08 +0200

    add all component into app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { RocketOutlined } from '@ant-design/icons';
+import TodoList from './components/todo-list/todo-list.component';
+import TodoForm from './components/todo-form/todo-form.component';
+
 import { AppContainer } from './app.styles';
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <AppContainer>
-      My todo list <RocketOutlined />
+      <h1>
+        My todo list <RocketOutlined />
+      </h1>
+      <Suspense fallback="Loading...">
+        <TodoList />
+      </Suspense>
+      <TodoForm />
     </AppContainer>
   );
 };
