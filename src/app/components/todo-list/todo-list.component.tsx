210afbb6b6df518ab34c36d7dceaf9b023f7c94b
Thomas Leveque <thomasl5895@gmail.com>
2020-04-25 19:54:36 +0200

    update todo-list with all logic

--- src/app/components/todo-list/todo-list.component.tsx
+++ src/app/components/todo-list/todo-list.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useFirestore, useFirestoreCollectionData } from 'reactfire';
+import { useFirestore, useFirestoreCollection } from 'reactfire';
 import TodoItem from '../todo-item/todo-item.component';
 import { ITodo } from '../../interfaces/todo.interface';
 
@@ -7,13 +7,25 @@ import { TodoListContainer } from './todo-list.styles';
 
 const TodoList: React.FC = () => {
   const firestore = useFirestore();
-  const todosRef = firestore.collection('todos');
-  const todos: ITodo[] = useFirestoreCollectionData(todosRef);
+  const todosRef: firebase.firestore.CollectionReference = firestore.collection('todos');
+  const snapshot: firebase.firestore.QuerySnapshot = useFirestoreCollection<null>(todosRef);
+  const todos: ITodo[] = snapshot.docs.map(
+    (doc: firebase.firestore.QueryDocumentSnapshot) => ({ id: doc.id, ...doc.data() } as ITodo)
+  );
   return (
     <TodoListContainer>
-      {todos.map((todo: ITodo, index: number) => (
-        <TodoItem key={index} todo={todo} />
-      ))}
+      <h2>Todo :</h2>
+      {todos
+        .filter(({ done }: ITodo) => !done)
+        .map((todo: ITodo, index: number) => {
+          return <TodoItem key={index} todo={todo} />;
+        })}
+      <h2>Done :</h2>
+      {todos
+        .filter(({ done }: ITodo) => done)
+        .map((todo: ITodo, index: number) => {
+          return <TodoItem key={index} todo={todo} />;
+        })}
     </TodoListContainer>
   );
 };
